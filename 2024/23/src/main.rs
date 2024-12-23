ccb1eb994d71241f164eff452e93de7786fe02f2
Maxwell Borden <Tacklebox@users.noreply.github.com>
2024-12-23 15:25:12 +0100

    23 part 2 works

--- 2024/23/src/main.rs
+++ 2024/23/src/main.rs
@@ -1,10 +1,12 @@
 use std::collections::HashSet;
 
+use anyhow::bail;
 use itertools::Itertools;
+use tracing::debug;
 
 const ADJ_SIZE: usize = 26 * 26;
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Hash)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord, Hash)]
 struct Clique {
     verticies: Vec<usize>,
 }
@@ -21,6 +23,17 @@ impl Clique {
             None
         }
     }
+    fn from_tuple(tuple: (usize, usize)) -> Self {
+        Self {
+            verticies: vec![tuple.0.min(tuple.1), tuple.0.max(tuple.1)],
+        }
+    }
+    fn len(&self) -> usize {
+        self.verticies.len()
+    }
+    fn format_members(&self) -> String {
+        self.verticies.iter().map(index_to_str).sorted().join(",")
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -29,33 +42,76 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn part1(input: Vec<String>) -> anyhow::Result<usize> {
-    let adjacency = construct_adjacency(&input);
+    let (adjacency, _) = construct_adjacency(&input);
     let n_t_triangles = count_t_triangles(&adjacency);
     Ok(n_t_triangles)
 }
 
-fn part2(input: Vec<String>) -> anyhow::Result<i64> {
-    let _ = input;
-    todo!();
+fn part2(input: Vec<String>) -> anyhow::Result<String> {
+    let (adjacency, verticies) = construct_adjacency(&input);
+    let mut cliques = clique_pairs(&input);
+    let mut clique_size = 3;
+    while cliques.len() > 1 {
+        cliques = grow_cliques(cliques, &adjacency, &verticies);
+        debug!("There are {} cliques of size {clique_size}", cliques.len());
+        clique_size += 1;
+    }
+    if let Some(clique) = cliques.into_iter().next() {
+        Ok(clique.format_members())
+    } else {
+        bail!("No unique maximum clique found")
+    }
+}
+
+fn grow_cliques(
+    cliques: HashSet<Clique>,
+    adjacency: &[Vec<usize>],
+    verticies: &[usize],
+) -> HashSet<Clique> {
+    let mut new_cliques = HashSet::new();
+    for mut clique in cliques {
+        for &vertex in verticies {
+            if let Some(new_clique) = clique.grow_with(vertex, adjacency) {
+                new_cliques.insert(new_clique);
+            }
+        }
+    }
+    new_cliques
+}
+
+fn clique_pairs(input: &[String]) -> HashSet<Clique> {
+    input
+        .iter()
+        .map(|line| line.split('-').map(str_to_index).next_tuple().unwrap())
+        .map(Clique::from_tuple)
+        .collect()
+}
+
+fn index_to_str(i: &usize) -> String {
+    let first_char = (((i / 26) as u8) + b'a') as char;
+    let second_char = (((i % 26) as u8) + b'a') as char;
+    format!("{first_char}{second_char}")
 }
 
 fn str_to_index(s: &str) -> usize {
     let sb = s.as_bytes();
     let index = ((sb[0] - b'a') as usize * 26) + ((sb[1] - b'a') as usize);
-    eprintln!("Index of {s} is {index}");
     index
 }
 
-fn construct_adjacency(input: &[String]) -> Vec<Vec<usize>> {
+fn construct_adjacency(input: &[String]) -> (Vec<Vec<usize>>, Vec<usize>) {
     let mut adjacency = vec![vec![0; ADJ_SIZE]; ADJ_SIZE];
+    let mut verticies = HashSet::new();
     for (a, b) in input
         .iter()
         .map(|line| line.split('-').map(str_to_index).next_tuple().unwrap())
     {
+        verticies.insert(a);
+        verticies.insert(b);
         adjacency[a][b] = 1;
         adjacency[b][a] = 1;
     }
-    adjacency
+    (adjacency, verticies.into_iter().collect_vec())
 }
 
 fn sorted_triple(a: usize, b: usize, c: usize) -> [usize; 3] {
